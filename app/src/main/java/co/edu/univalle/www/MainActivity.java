d416d53a8d4fbc006bccb96059301f3e95c643c4
desth-x <david.estrada@null.net>
2022-06-22 08:48:41 -0500

    User Creation GUI

--- app/src/main/java/co/edu/univalle/www/MainActivity.java
+++ app/src/main/java/co/edu/univalle/www/MainActivity.java
@@ -3,17 +3,11 @@ package co.edu.univalle.www;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
                     selectedFragment = new SearchFragment();
                     break;
                 case R.id.user:
-                    selectedFragment = new UserFragment();
+                    //selectedFragment = new UserFragment();
+                    selectedFragment = new userCreate();
                     break;
             }
             // It will help to replace the
